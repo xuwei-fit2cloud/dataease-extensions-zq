b1ecec8276b89c603655dbfce48d6f1641c59ce8
xuwei-fit2cloud <wei@fit2cloud.com>
2023-04-06 10:33:53 +0800

    update version

--- dataease-extensions-datasource/influxdb/build.sh
+++ dataease-extensions-datasource/influxdb/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mvn clean package -U -Dmaven.test.skip=true
 
-cp influxdb-backend/target/influxdb-backend-1.18.4-jar-with-dependencies.jar ./influxdb-backend-1.18.4.jar
+cp influxdb-backend/target/influxdb-backend-1.18.6-jar-with-dependencies.jar ./influxdb-backend-1.18.6.jar
 
-zip -r influxdb.zip  ./influxdb-backend-1.18.4.jar ./influxdbDriver ./plugin.json
+zip -r influxdb.zip  ./influxdb-backend-1.18.6.jar ./influxdbDriver ./plugin.json
